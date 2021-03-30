36e5f7c574f2e6a90ad70862dd1955149a3256ef
mariusberlin <51263484+mariusberlin@users.noreply.github.com>
2021-03-30 08:36:02 +0200

    Update vis.py

--- vis.py
+++ vis.py
@@ -1,26 +1,51 @@
+import tensorflow as tf
+import tensorflow.keras
 import matplotlib.gridspec as gridspec
+import ipywidgets as widgets
+from ipywidgets import interact, interactive, IntSlider, ToggleButtons
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
+%run a_methods.ipynb
+%run a_utils.ipynb
 
-#call the methods
+'''
+Jupyter notebook visualisation
+'''
 
-def vis(model_path,pixel_array):
+def vis(model_var,pixel_array):
+    
+    #if path is provided as model load model
+    if type(model) == str:
+        model_path = model_var
+        
+        #load model
+        trained_model = tf.keras.models.load_model(model_path, compile=False)
+        #get weights
+        loaded_weights = trained_model.get_weights()
+        #new_model.summary()
+        model.set_weights(loaded_weights)
+    
+    else:
+        pass
+    
+    #transform pixel array from (depth,length,width) to (1,depth,length,width,1)
+    if len(np.shape(pixel_array)) == 3:
+        pixel_array = np.expand_dims(pixel_array, 3)
+        pixel_array = np.expand_dims(pixel_array, 0)
 
-    #load model
-    trained_model = tf.keras.models.load_model(model_path, compile=False)
-    # get weights
-    loaded_weights = trained_model.get_weights()
-    # new_model.summary()
-    model.set_weights(loaded_weights)
-
-
+    #call the interpretability methods
     print("Calculating the saliency maps. This may take several minutes.")
-    capi_vsali = call_vsaliency(model,model_modifier,loss,pixel_array))
-    capi_sali = call_smooth(loss,pixel_array)
+    capi_vsali = call_vsaliency(model,model_modifier,loss,pixel_array)
+    capi_sali = call_smooth(model,model_modifier,loss,pixel_array)
     capi_grad = call_grad(model,model_modifier,loss,pixel_array)
     capi_gradplus = call_gradplus(model,model_modifier,loss,pixel_array)
     capi_faster_scorecam = call_faster_scorecam(model, model_modifier,loss,pixel_array)
 
 
+    #clear pritn statement
+    clear_output(wait=True)
 
+    #define the widgets
     layer = widgets.IntSlider(description='Slice:', min=0, max=(np.shape(pixel_array)[1] - 1), orientation='horizontal')
 
     method = widgets.ToggleButtons(
@@ -65,20 +90,22 @@ def vis(model_path,pixel_array):
         disabled=False
     )
     widgets.jslink((play, 'value'), (layer, 'value'))
-
+    
+    #assemble the widget
     ui = widgets.VBox([attention, method, layer, alpha, play])
 
-
+    #create the overlay of original image and heatmap
     def explore_3dimage(layer, attention, method, alpha) :
-        # print("Shape heatmap: ",np.shape(heatmap))
-
+        
+        #define the visualize
         if attention == "Slice-wise" :
             attention_mode = 'slice'
         # elif attention == "Average":
         #    attention_mode = 'mean'
         elif attention == "Max" :
             attention_mode = 'max'
-
+        
+        #load interpretability method
         if method == 'Vanilla Saliency' :
             heatmap = mode(attention_mode, capi_vsali)
             capi = capi_vsali
@@ -104,12 +131,12 @@ def vis(model_path,pixel_array):
         mri_image_slice = np.squeeze(pixel_array)[layer, :, :]
         mri_image_slice = np.float64(mri_image_slice)
 
-        # print(np.shape(heatmap))
 
-        # for mean/max cam only:
+        #max methods:
         if len(np.shape(heatmap)) == 2 :
             heatmap_slice = heatmap
-
+            
+        #slice methods
         elif len(np.shape(heatmap)) == 3 :
             heatmap_slice = np.squeeze(heatmap)[layer, :, :]
             heatmap_slice = np.float64(heatmap_slice)
@@ -118,11 +145,9 @@ def vis(model_path,pixel_array):
         ax1.imshow(heatmap_slice, cmap='jet', alpha=alpha)
         ax1.set_title(method + " - " + attention, fontsize=18)
         ax1.axis('off')
-
         ax2.set_xlabel('Slice', fontsize=13)
         ax2.set_ylabel('Pixel intensities', fontsize=13)
         ax2.set_title("Attention histogram", fontsize=18)
-        # ax.set_title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
 
         # calculate GAP in z-direction
         capi = np.squeeze(capi)
@@ -130,28 +155,15 @@ def vis(model_path,pixel_array):
 
         # normalize
         capi_gap_norm = (capi_gap - min(capi_gap)) / (max(capi_gap) - min(capi_gap))
-
         max_slice = np.argmax(capi_gap, axis=0)
         ax2.plot(np.squeeze(capi_gap))
-
-        # n, bins, patches = ax2.hist(b, 35, density=False
-
-        # txt = "Histrogram before clipping"
-        # fig.text(.5, -.04, txt, ha='center')
         plt.vlines(x=max_slice, ymin=0, ymax=max(capi_gap), linestyle="--")
         plt.text(max_slice + 0.5, 0.1 * max(capi_gap), "max slice: \n" + str(max_slice[0][0]))
-
         plt.vlines(x=layer, ymin=0, ymax=np.squeeze(capi_gap)[layer], linestyle="dotted", color="b")
         plt.text(layer + 0.5, 0.03 * max(capi_gap), "current slice: \n" + str(layer))
-
         plt.grid(axis=('both'), linestyle="--")
-
         xt = ax2.get_xticks()
-        # xt=np.append(int(xt),max_slice[0])
-        # xtl=xt.tolist()
-        # xtl[-1]= max_slice[0]
         plt.ylim(0)
-
         ax2.set_xticks(xt)
         ax2.set_xticklabels(xt)
         plt.xlim(left=0, right=(np.shape(pixel_array)[1] - 1))
@@ -159,10 +171,9 @@ def vis(model_path,pixel_array):
 
         return layer
 
-
+    #interactive output
     out = widgets.interactive_output(explore_3dimage,
                                      {'attention' : attention, 'method' : method, 'layer' : layer, 'alpha' : alpha})
+    #ensure smoother sliding through the layers
     out.layout.height = '550px'
-
     display(ui, out)
-
