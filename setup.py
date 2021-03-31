67846eef362e97bfea6b44217412d50cc3f8308a
mariusberlin <51263484+mariusberlin@users.noreply.github.com>
2021-03-31 11:48:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(
     author_email='pullig.marius@gmail.com',
     # Needed to actually package something
     packages=['xai_vis'],
+   python_requires='>=3.6, <3.9',
     # Needed for dependencies
-    install_requires=['numpy'],
+    install_requires=['numpy', 'ipywidgets']
+ 
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
